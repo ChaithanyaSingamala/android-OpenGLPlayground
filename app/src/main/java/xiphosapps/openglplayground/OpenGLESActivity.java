32d441a765350b9c8a4430e92d59d23690336b0d
Chaithanya K Singamala <cksignamala@gmail.com>
2018-03-01 19:32:42 +0530

    added code to fill screen with some color

---
+++ app/src/main/java/xiphosapps/openglplayground/OpenGLESActivity.java
@@ -0,0 +1,24 @@
+package xiphosapps.openglplayground;
+
+import android.opengl.GLSurfaceView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OpenGLESActivity extends AppCompatActivity {
+
+    private GLSurfaceView glSurfaceView;
+    private TestRenderer renderer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        renderer = new TestRenderer();
+
+        glSurfaceView = new GLSurfaceView(this);
+        glSurfaceView.setEGLContextClientVersion(2);
+        glSurfaceView.setRenderer(renderer);
+
+        setContentView(glSurfaceView);
+    }
+}
